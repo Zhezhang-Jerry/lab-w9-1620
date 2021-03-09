ea5f38b6503152c3bc5205eb59dd93c8d039fa4f
Jerry <zzhang155@my.bcit.ca>
2021-03-08 16:11:41 -0800

    add all the lab to the w9 repository

---
+++ app.js
@@ -0,0 +1,24 @@
+let num1 = 15
+let num2 = "15"
+
+if (num1 === num2) {
+    console.log("they match")
+}   else{
+    console.log("no luck")
+}
+
+let num1 = 10
+let num2 = 10
+
+if (num1 === num2) {
+    console.log("they match")
+}   else{
+    console.log("no luck")
+}
+
+const name = "Jerry"
+const bl = true
+
+for (const char of name) {
+    console.log(char)
+}
